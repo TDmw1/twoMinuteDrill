cc8238f83382731f41ff386ec98e89a18f75bbf6
TDmw1 <tdwitage@gmail.com>
2025-07-07 15:09:44 -0400

    Create overtime.h

---
+++ overtime.h
@@ -0,0 +1,10 @@
+#ifndef OVERTIME_H
+#define OVERTIME_H
+
+#include "player.h"
+
+// Function declaration for simplified overtime logic
+void handleOvertime();
+void handleOvertimeScenario(Player *player);
+
+#endif
