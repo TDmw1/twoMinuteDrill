7646a417adfa2de3122ae3cf6416b9ce40dca490
TDmw1 <tdwitage@gmail.com>
2025-07-07 15:09:16 -0400

    Create field_goal.h

---
+++ field_goal.h
@@ -0,0 +1,9 @@
+#ifndef FIELD_GOAL_H
+#define FIELD_GOAL_H
+
+#include "player.h"
+
+// Function declaration for field goal attempt. Determines if field goal is good or not.
+int attemptFieldGoal(Player *player);
+
+#endif
